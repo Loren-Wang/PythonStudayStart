9d29bf6dcd0dc91f176154f471c0cc4b0f64b910
LorenWang <wang.liang@ve-link.com>
2017-10-30 22:24:02 +0800

    列表循环操作

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,55 @@
-print("Hello World")
+# message = "hello world"
+# print(message)
+# message = "hello python world"
+# print(message)
+#
+# # 首字母大写
+# print("哈哈 abc def".title())
+# # 制表符
+# print("\tPython")
+# # 移除空白
+# # 移除首部空白
+# print("  Hello World ".lstrip())
+# # 移除尾部空白
+# print("  Hello World ".rstrip())
+# # 移除两侧空白
+# print("  Hello World ".strip())
+# # 移除首部H
+# print("HHHHello World ".lstrip("H"))
+# # 移除尾部d
+# print("Hello Worlddd".rstrip("d"))
+# # 移除两侧Hd
+# print("HHHHello Worldddd".strip("Hd"))
+# # 移除首部h
+# print("HHHHello World ".lstrip("h"))
+# # 移除尾部D
+# print("Hello Worlddd".rstrip("D"))
+# # 移除两侧hD
+# print("HHHHello Worldddd".strip("hD"))
+#
+#
+# print(10 * 0.1)
+#
+# # 删除列表元素
+# listContent = ['t','f','s']
+# popValue = listContent.pop(listContent.index('f'))
+# print(listContent)
+# print(popValue)
+
+# 列表循环输出
+animals = ['cat','dog','pig']
+for animal in animals:
+    print(animal)
+print('\n')
+
+for animal in animals:
+    print(animal)
+    print(animal)
+print('\n')
+
+for animal in animals:{
+    print(animal),
+    print(animal),
+    print(animal)
+}
+print('\n')
